8f72df9186e65a2cc531df7f150821fe905369f7
Jinay Madhavi <jinaymadhavi74@gmail.com>
2026-08-17 20:18:53 +0530

    print statement

---
+++ src/Basic/PrintStatement.java
@@ -0,0 +1,18 @@
+package Basic;
+
+public class PrintStatement {
+	public static void main(String[] args) {
+		
+		System.out.print("Hello World");
+		System.out.println(" Hello java ");
+		
+		System.out.println("*\n**\n***\n****\n*****");
+		
+		System.out.println("*****");
+		System.out.println("****");
+		System.out.println("***");
+		System.out.println("**");
+		System.out.println("*");
+		
+	}
+}
